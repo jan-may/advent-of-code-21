a8ef00c9333cb87b6ae6f9a5b95de5869ffb736a
jan-may <78799602+jan-may@users.noreply.github.com>
2021-12-02 07:08:57 +0100

    Add files via upload

---
+++ advent_of_code_21/02/02.py
@@ -0,0 +1,49 @@
+filepath = r"...\advent_of_code_21\02\input.txt"
+
+def readFile(fileName):
+	with open(fileName, "r") as fileObj:
+		words = fileObj.read().splitlines()
+	# words = [int(i) for i in words] #transform to int list
+	return words
+
+input = readFile(filepath)
+
+# task1
+horizontal = 0
+depth = 0
+
+for i in input:
+	i = i.split()
+	position = i[0]
+	value = int(i[1])
+	if position == "down":
+		depth += value
+	elif position == "up":
+		depth -= value
+	elif position == "forward":
+		horizontal += value
+erg = horizontal * depth
+print(erg)
+
+# Task 2
+aim = 0
+depth = 0
+horizontal = 0
+
+for i in input:
+	i = i.split()
+	position = i[0]
+	value = int(i[1])
+	if position == "down":
+		aim += value
+	elif position == "up":
+		aim -= value
+	elif position == "forward":
+		if aim > 0:
+		    depth += aim * value
+		horizontal += value
+
+erg = horizontal * depth
+print(erg)
+
+
