8619555a4216a3e19889901f95f7fc3607a36775
jan-may <78799602+jan-may@users.noreply.github.com>
2021-12-02 07:09:30 +0100

    Update 02.py

--- advent_of_code_21/02/02.py
+++ advent_of_code_21/02/02.py
@@ -3,7 +3,6 @@ filepath = r"...\advent_of_code_21\02\input.txt"
 def readFile(fileName):
 	with open(fileName, "r") as fileObj:
 		words = fileObj.read().splitlines()
-	# words = [int(i) for i in words] #transform to int list
 	return words
 
 input = readFile(filepath)
